d439ae3a51573d7549ee38f5c011853341b4947a
Stevanus Prasetyo Soemadi <stevanus.pras@gmail.com>
2020-07-29 18:35:52 +0700

    first setup project

---
+++ TixDota2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TixDota2
+//
+//  Created by Stevanus Prasetyo Soemadi on 29/07/20.
+//  Copyright © 2020 Stevanus Prasetyo Soemadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
