8743d150c0a9af87e658a8c2124a00bc2b339de5
Stevanus Prasetyo Soemadi <stevanus.pras@gmail.com>
2020-08-03 21:29:50 +0700

    setup keychain storage

---
+++ TixDota2/Storage/HeroStorage.swift
@@ -0,0 +1,9 @@
+//
+//  HeroStorage.swift
+//  TixDota2
+//
+//  Created by Stevanus Prasetyo Soemadi on 03/08/20.
+//  Copyright © 2020 Stevanus Prasetyo Soemadi. All rights reserved.
+//
+
+import Foundation
